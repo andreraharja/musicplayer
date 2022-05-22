c89feb608d2fa662c9bdb61e6432937dcacdfcf8
it_andre <andre.raharja@mayora.co.id>
2022-05-22 22:09:27 +0700

    unit testing

--- test/home_page_api_test.dart
+++ test/home_page_api_test.dart
@@ -2,14 +2,14 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:musicplayer/api/home_page_api.dart';
 import 'package:musicplayer/model/home_page_model.dart';
 
-void main() async {
+void main() {
+  final client = HomePageApi();
+
   test('test init Data Music', () async {
-    final client = HomePageApi();
     expect(await client.initDataMusic(), isA<List<DataMusic>>());
   });
 
   test('test Search Data Music', () async {
-    final client = HomePageApi();
     expect(await client.getDataMusic("Owl City"), isA<List<DataMusic>>());
   });
 }

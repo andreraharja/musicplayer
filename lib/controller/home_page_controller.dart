a59e988be9fd0cad3f264568cf33dc2ea72e57cd
it_andre <andre.raharja@mayora.co.id>
2022-05-22 14:22:29 +0700

    add readme

--- lib/controller/home_page_controller.dart
+++ lib/controller/home_page_controller.dart
@@ -5,26 +5,53 @@ import 'package:musicplayer/api/home_page_api.dart';
 import 'package:musicplayer/model/home_page_model.dart';
 
 class HomePageController extends GetxController {
+  ///variable for field search
+  TextEditingController searchText = TextEditingController();
+
+  ///variable for save list data music from api
   var dataMusic = List<DataMusic>.empty().obs;
   var resultDataMusic = List<DataMusic>.empty().obs;
+
+  ///variable for selected music
   var selectedMusic = DataMusic().obs;
-  var isLoading = true.obs;
-  var isTap = false.obs;
+
+  ///flag for selected music
   var isPlay = List<bool>.empty().obs;
+
+  ///flag for loading
+  var isLoading = true.obs;
+
+  ///flag for show / hide controlWidget
+  var isTap = false.obs;
+
+  ///flag for pause / resume music
   var isPause = false.obs;
+
+  ///variable temporary for save index that play before
   var tempIndex = 0;
+
+  ///variable for control duration
   var maxDuration = 0.obs;
   var currentPosition = 0.obs;
   var currentPositionLabel = "00:00".obs;
   var maxDurationLabel = "00:00".obs;
-  TextEditingController searchText = TextEditingController();
+
+  ///initialize library audioplayers
   AudioPlayer audioPlayer = AudioPlayer();
 
   @override
   void onInit() async {
+
+    ///init list data music from Api iTunes
+    ///dataMusic will use for backup data after user use search feature
     dataMusic.value = await initDataMusic();
+
+    ///store to resultDataMusic, resultDataMusic will use for display in UI
     resultDataMusic.addAll(dataMusic);
+
+    ///generate list boolean for flag each song
     isPlay.value = List.generate(resultDataMusic.length, (i) => false);
+
     isLoading(false);
     super.onInit();
   }
@@ -55,47 +82,57 @@ class HomePageController extends GetxController {
       tempIndex = index;
       selectedMusic.value = dataMusic;
 
-      audioPlayer.onDurationChanged.listen((Duration d) {
-        maxDuration.value = d.inMilliseconds;
-
-        int shours = Duration(milliseconds: maxDuration.value).inHours;
-        int sminutes = Duration(milliseconds: maxDuration.value).inMinutes;
-        int sseconds = Duration(milliseconds: maxDuration.value).inSeconds;
-
-        int rhours = shours;
-        int rminutes = sminutes - (shours * 60);
-        int rseconds = sseconds - (sminutes * 60 + shours * 60 * 60);
-
-        if (rhours == 0) {
-          maxDurationLabel.value = "$rminutes:$rseconds";
-        } else {
-          maxDurationLabel.value = "$rhours:$rminutes:$rseconds";
-        }
-      });
-
-      audioPlayer.onAudioPositionChanged.listen((Duration p) {
-        currentPosition.value = p.inMilliseconds;
-
-        int shours = Duration(milliseconds: currentPosition.value).inHours;
-        int sminutes = Duration(milliseconds: currentPosition.value).inMinutes;
-        int sseconds = Duration(milliseconds: currentPosition.value).inSeconds;
-
-        int rhours = shours;
-        int rminutes = sminutes - (shours * 60);
-        int rseconds = sseconds - (sminutes * 60 + shours * 60 * 60);
-
-        if (rhours == 0) {
-          currentPositionLabel.value = "$rminutes:$rseconds";
-        } else {
-          currentPositionLabel.value = "$rhours:$rminutes:$rseconds";
-        }
-      });
-
-      audioPlayer.onPlayerCompletion.listen((event) {
-        isPause(true);
-        isPlay[index] = false;
-      });
+      getMaxDuration();
+      getAudioPosition();
+      getAudioComplete(index);
     }
+  }
+
+  void getMaxDuration() {
+    audioPlayer.onDurationChanged.listen((Duration d) {
+      maxDuration.value = d.inMilliseconds;
+
+      int shours = Duration(milliseconds: maxDuration.value).inHours;
+      int sminutes = Duration(milliseconds: maxDuration.value).inMinutes;
+      int sseconds = Duration(milliseconds: maxDuration.value).inSeconds;
+
+      int rhours = shours;
+      int rminutes = sminutes - (shours * 60);
+      int rseconds = sseconds - (sminutes * 60 + shours * 60 * 60);
+
+      if (rhours == 0) {
+        maxDurationLabel.value = "$rminutes:$rseconds";
+      } else {
+        maxDurationLabel.value = "$rhours:$rminutes:$rseconds";
+      }
+    });
+  }
+
+  void getAudioPosition() {
+    audioPlayer.onAudioPositionChanged.listen((Duration p) {
+      currentPosition.value = p.inMilliseconds;
+
+      int shours = Duration(milliseconds: currentPosition.value).inHours;
+      int sminutes = Duration(milliseconds: currentPosition.value).inMinutes;
+      int sseconds = Duration(milliseconds: currentPosition.value).inSeconds;
+
+      int rhours = shours;
+      int rminutes = sminutes - (shours * 60);
+      int rseconds = sseconds - (sminutes * 60 + shours * 60 * 60);
+
+      if (rhours == 0) {
+        currentPositionLabel.value = "$rminutes:$rseconds";
+      } else {
+        currentPositionLabel.value = "$rhours:$rminutes:$rseconds";
+      }
+    });
+  }
+
+  void getAudioComplete(index) {
+    audioPlayer.onPlayerCompletion.listen((event) {
+      isPause(true);
+      isPlay[index] = false;
+    });
   }
 
   void seekMusic(value) async {

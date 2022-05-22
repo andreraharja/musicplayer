ac5223e370535bdd13074de46925457969a43982
it_andre <andre.raharja@mayora.co.id>
2022-05-22 18:09:15 +0700

    unit testing

--- test/home_page_api_test.dart
+++ test/home_page_api_test.dart
@@ -1,28 +1,15 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:musicplayer/api/home_page_api.dart';
-import 'package:mockito/annotations.dart';
-import 'package:mockito/mockito.dart';
 import 'package:musicplayer/model/home_page_model.dart';
 
-@GenerateMocks([HomePageApi], customMocks: [MockSpec<HomePageApi>(returnNullOnMissingStub: true)])
-Future<void> main() async {
-  var homePageApi = MockHomePageApi();
-
-  test("returns init data music", () async {
-    const model = List<DataMusic>;
-    when(homePageApi.initDataMusic().then((_) async {
-      return model;
-    }));
-    final res = homePageApi.initDataMusic();
-    expect(res, isA<List<DataMusic>>());
-    expect(res, model);
+void main() async {
+  test('test init Data Music', () async {
+    final client = HomePageApi();
+    expect(await client.initDataMusic(), isA<List<DataMusic>>());
   });
 
-  test('return init data music throws Exception', () {
-    when(homePageApi.initDataMusic().then((_) async {
-      throw Exception();
-    }));
-    final res = homePageApi.initDataMusic();
-    expect(res, throwsException);
+  test('test Search Data Music', () async {
+    final client = HomePageApi();
+    expect(await client.getDataMusic("Owl City"), isA<List<DataMusic>>());
   });
 }
